34ae341ea234d9259e08907558a8fc164c18d705
Andreas <andi@aimeos.org>
2015-08-11 12:19:16 +0200

    Added customer group manager

---
+++ lib/custom/src/MShop/Customer/Manager/Group/Laravel.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015
+ * @package MShop
+ * @subpackage Customer
+ */
+
+
+/**
+ * Laravel implementation of the customer group class.
+ *
+ * @package MShop
+ * @subpackage Customer
+ */
+class MShop_Customer_Manager_Group_Laravel
+	extends MShop_Customer_Manager_Group_Default
+	implements MShop_Customer_Manager_Group_Interface
+{
+}
