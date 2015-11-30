11d3c4dd0c282b72ff97c7c3b67ae5dde7bfdf33
Aimeos <aimeos@aimeos.org>
2015-11-30 19:17:23 +0100

    Implemented Laravel file system manager

---
+++ lib/custom/src/MW/Filesystem/Manager/Laravel.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015
+ * @package MW
+ * @subpackage Filesystem
+ */
+
+
+namespace Aimeos\MW\Filesystem\Manager;
+
+
+/**
+ * Laravel file system manager
+ *
+ * @package MW
+ * @subpackage Filesystem
+ */
+class Laravel extends Standard implements Iface
+{
+	private $objects = array();
+	private $fsm;
+
+
+	/**
+	 * Initializes the object
+	 *
+	 * @param \Illuminate\Filesystem\FilesystemManager $fsm Laravel file system manager object
+	 * @param \Aimeos\MW\Config\Iface $config Configuration object
+	 */
+	public function __construct( \Illuminate\Filesystem\FilesystemManager $fsm, \Aimeos\MW\Config\Iface $config )
+	{
+		parent::__construct( $config );
+
+		$this->fsm = $fsm;
+	}
+
+
+	/**
+	 * Returns the file system for the given name
+	 *
+	 * @param string $name Key for the file system
+	 * @return \Aimeos\MW\Filesystem\Iface File system object
+	 * @throws \Aimeos\MW\Filesystem\Exception If an no configuration for that name is found
+	 */
+	public function get( $name )
+	{
+		$key = $this->getConfig( $name );
+
+		if( is_string( $key ) )
+		{
+			if( !isset( $this->objects[$key] ) ) {
+				$this->objects[$key] = new \Aimeos\MW\Filesystem\Laravel( $this->fsm->disk( $key ) );
+			}
+
+			return $this->objects[$key];
+		}
+
+		return parent::get( $name );
+	}
+}
---
+++ lib/custom/tests/MW/Filesystem/Manager/LaravelTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Aimeos\MW\Filesystem\Manager;
+
+
+class LaravelTest extends \PHPUnit_Framework_TestCase
+{
+	private $config;
+	private $object;
+	private $storage;
+
+
+	protected function setUp()
+	{
+		if( !class_exists( '\Illuminate\Filesystem\FilesystemManager' ) ) {
+			$this->markTestSkipped( 'Install the Laravel framework first' );
+		}
+
+		$this->storage = $this->getMockBuilder( '\Illuminate\Filesystem\FilesystemManager' )
+			->setMethods( array( 'get' ) )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->config = new \Aimeos\MW\Config\PHPArray( array(), array() );
+		$this->object = new \Aimeos\MW\Filesystem\Manager\Laravel( $this->storage, $this->config );
+	}
+
+
+	protected function tearDown()
+	{
+		$this->config->set( 'resource/fs-media', null );
+		$this->config->set( 'resource/fs', null );
+
+		unset( $this->object, $this->storage );
+	}
+
+
+	public function testGet()
+	{
+		$fs = $this->getMockBuilder( 'Illuminate\Contracts\Filesystem\Filesystem' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->storage->expects( $this->once() )->method( 'get' )
+			->will( $this->returnValue( $fs ) );
+
+		$this->config->set( 'resource/fs-media', 'local' );
+		$this->assertInstanceof( 'Aimeos\MW\Filesystem\Iface', $this->object->get( 'fs-media' ) );
+	}
+
+
+	public function testGetFallback()
+	{
+		$this->config->set( 'resource/fs', array( 'adapter' => 'Standard', 'basedir' => __DIR__ ) );
+		$this->assertInstanceof( 'Aimeos\MW\Filesystem\Iface', $this->object->get( 'fs-media' ) );
+	}
+
+
+	public function testGetException()
+	{
+		$this->setExpectedException( 'Aimeos\MW\Filesystem\Exception' );
+		$this->object->get( 'fs-media' );
+	}
+}
