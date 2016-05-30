9e26560b20c9761ee64a3cec7ef141f3b4927835
Aimeos <aimeos@aimeos.org>
2016-05-30 14:07:48 +0200

    Added SQL statements for retrieving inserted IDs

--- lib/custom/config/mshop/customer/manager/lists/laravel.php
+++ lib/custom/config/mshop/customer/manager/lists/laravel.php
@@ -98,6 +98,12 @@ return array(
 		',
 	),
 	'newid' => array(
-		'mysql' => 'SELECT LAST_INSERT_ID()'
+		'db2' => 'SELECT IDENTITY_VAL_LOCAL()',
+		'mysql' => 'SELECT LAST_INSERT_ID()',
+		'oracle' => 'SELECT users_list.CURRVAL FROM DUAL',
+		'pgsql' => 'SELECT lastval()',
+		'sqlite' => 'SELECT last_insert_rowid()',
+		'sqlsrv' => 'SELECT SCOPE_IDENTITY()',
+		'sqlanywhere' => 'SELECT @@IDENTITY',
 	),
 );
--- lib/custom/config/mshop/customer/manager/lists/type/laravel.php
+++ lib/custom/config/mshop/customer/manager/lists/type/laravel.php
@@ -54,6 +54,12 @@ return array(
 		',
 	),
 	'newid' => array(
-		'mysql' => 'SELECT LAST_INSERT_ID()'
+		'db2' => 'SELECT IDENTITY_VAL_LOCAL()',
+		'mysql' => 'SELECT LAST_INSERT_ID()',
+		'oracle' => 'SELECT users_list_type.CURRVAL FROM DUAL',
+		'pgsql' => 'SELECT lastval()',
+		'sqlite' => 'SELECT last_insert_rowid()',
+		'sqlsrv' => 'SELECT SCOPE_IDENTITY()',
+		'sqlanywhere' => 'SELECT @@IDENTITY',
 	),
 );
