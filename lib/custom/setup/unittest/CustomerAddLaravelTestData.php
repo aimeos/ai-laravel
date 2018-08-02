65864b7510864dc36a5edd99bb20beba2cee422d
Aimeos <aimeos@aimeos.org>
2018-08-03 00:26:20 +0200

    Uses checkClass() method

--- lib/custom/setup/unittest/CustomerAddLaravelTestData.php
+++ lib/custom/setup/unittest/CustomerAddLaravelTestData.php
@@ -30,10 +30,7 @@ class CustomerAddLaravelTestData extends \Aimeos\MW\Setup\Task\CustomerAddTestDa
 	 */
 	public function migrate()
 	{
-		$iface = '\\Aimeos\\MShop\\Context\\Item\\Iface';
-		if( !( $this->additional instanceof $iface ) ) {
-			throw new \Aimeos\MW\Setup\Exception( sprintf( 'Additionally provided object is not of type "%1$s"', $iface ) );
-		}
+		\Aimeos\MW\Common\Base::checkClass( '\\Aimeos\\MShop\\Context\\Item\\Iface', $this->additional );
 
 		$this->msg( 'Adding Laravel customer test data', 0 );
 		$this->additional->setEditor( 'ai-laravel:unittest' );
--- lib/custom/tests/MShop/Customer/Manager/Property/Type/LaravelTest.php
+++ lib/custom/tests/MShop/Customer/Manager/Property/Type/LaravelTest.php
@@ -83,7 +83,7 @@ class LaravelTest extends \PHPUnit\Framework\TestCase
 
 	public function testSaveInvalid()
 	{
-		$this->setExpectedException( '\Aimeos\MShop\Exception' );
+		$this->setExpectedException( '\Aimeos\MW\Common\Exception' );
 		$this->object->saveItem( new \Aimeos\MShop\Locale\Item\Standard() );
 	}
 
