8d4739c7c02998ad1d5a019ec971275c6f9ced10
Aimeos <aimeos@aimeos.org>
2018-10-02 18:30:42 +0200

    Copyright update

--- lib/custom/tests/MShop/Customer/Manager/Lists/Type/LaravelTest.php
+++ lib/custom/tests/MShop/Customer/Manager/Lists/Type/LaravelTest.php
@@ -5,7 +5,7 @@ namespace Aimeos\MShop\Customer\Manager\Lists\Type;
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2015-2017
+ * @copyright Aimeos (aimeos.org), 2015-2018
  */
 class LaravelTest extends \PHPUnit\Framework\TestCase
 {
