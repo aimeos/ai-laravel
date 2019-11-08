6f4ccd253dc6f05c13798d60be4ffe5b5beaa82f
Aimeos <aimeos@aimeos.org>
2019-11-08 11:14:04 +0100

    Delete passed items and removed referenced items

--- lib/custom/src/MShop/Customer/Manager/Laravel.php
+++ lib/custom/src/MShop/Customer/Manager/Laravel.php
@@ -321,14 +321,15 @@ class Laravel
 
 
 	/**
-	 * Removes multiple items specified by ids in the array.
+	 * Removes multiple items.
 	 *
-	 * @param array $ids List of IDs
+	 * @param \Aimeos\MShop\Common\Item\Iface[]|string[] $itemIds List of item objects or IDs of the items
+	 * @return \Aimeos\MShop\Common\Manager\Iface Manager object for chaining method calls
 	 */
-	public function deleteItems( array $ids )
+	public function deleteItems( array $itemIds )
 	{
 		$path = 'mshop/customer/manager/laravel/delete';
-		$this->deleteItemsBase( $ids, $path, false );
+		return $this->deleteItemsBase( $itemIds, $path, false )->deleteRefItems( $itemIds );
 	}
 
 
