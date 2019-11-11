2686fcd963e0b4950c3d946abc4eb126dc66d720
Aimeos <aimeos@aimeos.org>
2019-11-11 09:26:13 +0100

    Use PHP 7 type hints in logger classes

--- lib/custom/src/MW/Logger/Laravel5.php
+++ lib/custom/src/MW/Logger/Laravel5.php
@@ -38,57 +38,23 @@ class Laravel5
 	/**
 	 * Writes a message to the configured log facility.
 	 *
-	 * @param string $message Message text that should be written to the log facility
-	 * @param integer $priority Priority of the message for filtering
+	 * @param string|array|object $message Message text that should be written to the log facility
+	 * @param int $prio Priority of the message for filtering
 	 * @param string $facility Facility for logging different types of messages (e.g. message, auth, user, changelog)
+	 * @return \Aimeos\MW\Logger\Iface Logger object for method chaining
 	 * @throws \Aimeos\MW\Logger\Exception If an error occurs in Zend_Log
 	 * @see \Aimeos\MW\Logger\Base for available log level constants
 	 */
-	public function log( $message, $priority = \Aimeos\MW\Logger\Base::ERR, $facility = 'message' )
+	public function log( $message, int $priority = Base::ERR, string $facility = 'message' ) : Iface
 	{
-		try
-		{
-			if( !is_scalar( $message ) ) {
-				$message = json_encode( $message );
-			}
-
-			$this->logger->log( $message, $this->translatePriority( $priority ) );
+		if( !is_scalar( $message ) ) {
+			$message = json_encode( $message );
 		}
-		catch( \Exception $e ) {
-			throw new \Aimeos\MW\Logger\Exception( $e->getMessage(), $e->getCode(), $e );
-		}
-	}
 
-
-	/**
-	 * Translates the log priority to the log levels of Monolog.
-	 *
-	 * @param integer $priority Log level from \Aimeos\MW\Logger\Base
-	 * @return integer Log level from Monolog\Logger
-	 * @throws \Aimeos\MW\Logger\Exception If log level is unknown
-	 */
-	protected function translatePriority( $priority )
-	{
-		switch( $priority )
-		{
-			case \Aimeos\MW\Logger\Base::EMERG:
-				return 'emergency';
-			case \Aimeos\MW\Logger\Base::ALERT:
-				return 'alert';
-			case \Aimeos\MW\Logger\Base::CRIT:
-				return 'critical';
-			case \Aimeos\MW\Logger\Base::ERR:
-				return 'error';
-			case \Aimeos\MW\Logger\Base::WARN:
-				return 'warning';
-			case \Aimeos\MW\Logger\Base::NOTICE:
-				return 'notice';
-			case \Aimeos\MW\Logger\Base::INFO:
-				return 'info';
-			case \Aimeos\MW\Logger\Base::DEBUG:
-				return 'debug';
-			default:
-				throw new \Aimeos\MW\Logger\Exception( 'Invalid log level' );
+		try {
+			$this->logger->log( $message, $this->getLogLevel( $priority ) );
+		} catch( \Exception $e ) {
+			throw new Exception( $e->getMessage(), $e->getCode(), $e );
 		}
 	}
 }
