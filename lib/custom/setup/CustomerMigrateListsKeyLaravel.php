3443ec7f748b62fb24364a9b2fd2a4c017808c05
Aimeos <aimeos@aimeos.org>
2019-12-18 19:08:25 +0100

    Migrates users_list key column

---
+++ lib/custom/setup/CustomerMigrateListsKeyLaravel.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2019
+ */
+
+
+namespace Aimeos\MW\Setup\Task;
+
+
+/**
+ * Updates key columns
+ */
+class CustomerMigrateListsKeyLaravel extends TablesMigrateListsKey
+{
+	/**
+	 * Returns the list of task names which this task depends on.
+	 *
+	 * @return string[] List of task names
+	 */
+	public function getPreDependencies()
+	{
+		return ['TablesMigrateListsKey'];
+	}
+
+
+	/**
+	 * Executes the task
+	 */
+	public function migrate()
+	{
+		$this->msg( 'Update Laravel lists "key" columns', 0 ); $this->status( '' );
+
+		$this->process( ['db-customer' => 'users_list'] );
+	}
+}
