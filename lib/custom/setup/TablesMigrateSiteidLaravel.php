63323522bcc8f1e522c0c9c3c278e8562b19c73a
Aimeos <aimeos@aimeos.org>
2019-12-19 06:18:32 +0100

    Added setup task to migrate site IDs

---
+++ lib/custom/setup/TablesMigrateSiteidLaravel.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2019
+ */
+
+
+namespace Aimeos\MW\Setup\Task;
+
+
+/**
+ * Updates site ID columns
+ */
+class TablesMigrateSiteidLaravel extends TablesMigrateSiteid
+{
+	private $resources = [
+		'db-customer' => [
+			'users_list_type', 'users_property_type',
+			'users_property', 'users_list', 'users_address', 'users',
+		],
+	];
+
+
+	/**
+	 * Returns the list of task names which this task depends on.
+	 *
+	 * @return string[] List of task names
+	 */
+	public function getPreDependencies() : array
+	{
+		return ['TablesMigrateSiteid'];
+	}
+
+
+	/**
+	 * Returns the list of task names which this task depends on.
+	 *
+	 * @return string[] List of task names
+	 */
+	public function getPostDependencies() : array
+	{
+		return ['TablesCreateMShop'];
+	}
+
+
+	/**
+	 * Executes the task
+	 */
+	public function migrate()
+	{
+		$this->msg( 'Update Laravel "siteid" columns', 0, '' );
+
+		$this->process( $this->resources );
+	}
+}
