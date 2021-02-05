f626a06b551eb5545fd1b5c40fa5617d5ff04ba4
Aimeos <aimeos@aimeos.org>
2021-02-05 12:29:09 +0100

    Support multiple keys to group by in aggregate()

--- lib/custom/config/mshop/customer.php
+++ lib/custom/config/mshop/customer.php
@@ -213,28 +213,28 @@ return array(
 			'laravel' => array(
 				'aggregate' => array(
 					'ansi' => '
-						SELECT "key", COUNT("id") AS "count"
+						SELECT :keys, COUNT("id") AS "count"
 						FROM (
-							SELECT :key AS "key", lvuli."id" AS "id"
+							SELECT :acols, lvuli."id" AS "id"
 							FROM "users_list" AS lvuli
 							:joins
 							WHERE :cond
 							ORDER BY :order
 							OFFSET :start ROWS FETCH NEXT :size ROWS ONLY
 						) AS list
-						GROUP BY "key"
+						GROUP BY :keys
 					',
 					'mysql' => '
-						SELECT "key", COUNT("id") AS "count"
+						SELECT :keys, COUNT("id") AS "count"
 						FROM (
-							SELECT :key AS "key", lvuli."id" AS "id"
+							SELECT :acols, lvuli."id" AS "id"
 							FROM "users_list" AS lvuli
 							:joins
 							WHERE :cond
 							ORDER BY :order
 							LIMIT :size OFFSET :start
 						) AS list
-						GROUP BY "key"
+						GROUP BY :keys
 					',
 				),
 				'delete' => array(
