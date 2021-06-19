40d2571fbea701ac275f2139d73855d662171f8e
Aimeos <aimeos@aimeos.org>
2021-06-19 09:25:44 +0200

    Minor coding style improvement

--- manifest.php
+++ manifest.php
@@ -2,21 +2,21 @@
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2015-2021
+ * @copyright Aimeos (aimeos.org], 2015-2021
  */
 
-return array(
+return [
 	'name' => 'ai-laravel',
-	'depends' => array(
+	'depends' => [
 		'aimeos-core',
-	),
-	'include' => array(
+	],
+	'include' => [
 		'lib/custom/src',
-	),
-	'config' => array(
+	],
+	'config' => [
 		'lib/custom/config',
-	),
-	'setup' => array(
+	],
+	'setup' => [
 		'lib/custom/setup',
-	),
-);
+	],
+];
