45676f22fbf01f13c65420f77cfd5dd913fa7038
Aimeos <aimeos@aimeos.org>
2022-03-22 21:14:59 +0100

    Renamed context item

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -48,7 +48,7 @@ class TestHelper
 
 	private static function createContext( $site )
 	{
-		$ctx = new \Aimeos\MShop\Context\Item\Standard();
+		$ctx = new \Aimeos\MShop\Context();
 		$aimeos = self::getAimeos();
 
 
