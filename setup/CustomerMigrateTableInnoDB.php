e38f866b300f0631e1505d793abca6937fde5232
Aimeos <aimeos@aimeos.org>
2022-07-16 08:30:01 +0200

    Enforces InnoDB for users table

---
+++ setup/CustomerMigrateTableInnoDB.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2022
+ */
+
+
+namespace Aimeos\Upscheme\Task;
+
+
+/**
+ * Changes the table type to InnoDB
+ */
+class CustomerMigrateTableInnoDB extends Base
+{
+	/**
+	 * Returns the list of task names which depends on this task.
+	 *
+	 * @return array List of task names
+	 */
+	public function before() : array
+	{
+		return ['Customer'];
+	}
+
+
+	/**
+	 * Executes the task
+	 */
+	public function up()
+	{
+		$db = $this->db( 'db-customer' );
+
+		if( !$db->hasTable( 'users' ) ) {
+			return;
+		}
+
+		$this->info( sprintf( 'Migrate users table engine to InnoDB' ), 'vv' );
+
+		$db->for( 'mysql', 'ALTER TABLE users ENGINE=InnoDB' );
+	}
+}
