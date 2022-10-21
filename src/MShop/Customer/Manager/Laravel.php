10a6df162b188c337b3cec4d0ee31d416a59b08d
Aimeos <aimeos@aimeos.org>
2022-10-21 10:55:51 +0200

    Allow inserting items into sub-sites

--- src/MShop/Customer/Manager/Laravel.php
+++ src/MShop/Customer/Manager/Laravel.php
@@ -554,7 +554,7 @@ class Laravel
 				$stmt->bind( $idx, $id, \Aimeos\Base\DB\Statement\Base::PARAM_INT );
 				$item->setId( $id );
 			} else {
-				$stmt->bind( $idx++, $context->locale()->getSiteId() );
+				$stmt->bind( $idx++, $this->siteId( $item->getSiteId(), \Aimeos\MShop\Locale\Manager\Base::SITE_SUBTREE ) );
 				$stmt->bind( $idx, $date ); // Creation time
 			}
 
