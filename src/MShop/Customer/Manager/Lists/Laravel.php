39aa9a8eb2acb526749b5034703b06b77c5f032b
Aimeos <aimeos@aimeos.org>
2024-06-11 07:59:13 +0200

    Simplified customer lists manager

--- src/MShop/Customer/Manager/Lists/Laravel.php
+++ src/MShop/Customer/Manager/Lists/Laravel.php
@@ -20,136 +20,6 @@ namespace Aimeos\MShop\Customer\Manager\Lists;
 class Laravel
 	extends \Aimeos\MShop\Customer\Manager\Lists\Standard
 {
-	private array $searchConfig = array(
-		'customer.lists.id' => array(
-			'code' =>'customer.lists.id',
-			'internalcode' =>'mcusli."id"',
-			'internaldeps' => array( 'LEFT JOIN "users_list" AS mcusli ON ( mcus."id" = mcusli."parentid" )' ),
-			'label' =>'Customer list ID',
-			'type' => 'int',
-			'public' => false,
-		),
-		'customer.lists.siteid' => array(
-			'code' =>'customer.lists.siteid',
-			'internalcode' =>'mcusli."siteid"',
-			'label' =>'Customer list site ID',
-			'type' => 'string',
-			'public' => false,
-		),
-		'customer.lists.parentid' => array(
-			'code' =>'customer.lists.parentid',
-			'internalcode' =>'mcusli."parentid"',
-			'label' =>'Customer list parent ID',
-			'type' => 'int',
-			'public' => false,
-		),
-		'customer.lists.key' => array(
-			'code' => 'customer.lists.key',
-			'internalcode' => 'mcusli."key"',
-			'label' => 'Unique key',
-			'type' => 'string',
-			'public' => false,
-		),
-		'customer.lists.domain' => array(
-			'code' =>'customer.lists.domain',
-			'internalcode' =>'mcusli."domain"',
-			'label' =>'Customer list domain',
-			'type' => 'string',
-		),
-		'customer.lists.type' => array(
-			'code' =>'customer.lists.type',
-			'internalcode' =>'mcusli."type"',
-			'label' =>'Customer list type',
-			'type' => 'string',
-		),
-		'customer.lists.refid' => array(
-			'code' =>'customer.lists.refid',
-			'internalcode' =>'mcusli."refid"',
-			'label' =>'Customer list reference ID',
-			'type' => 'string',
-		),
-		'customer.lists.datestart' => array(
-			'code' =>'customer.lists.datestart',
-			'internalcode' =>'mcusli."start"',
-			'label' =>'Customer list start date/time',
-			'type' => 'datetime',
-		),
-		'customer.lists.dateend' => array(
-			'code' =>'customer.lists.dateend',
-			'internalcode' =>'mcusli."end"',
-			'label' =>'Customer list end date/time',
-			'type' => 'datetime',
-		),
-		'customer.lists.config' => array(
-			'code' =>'customer.lists.config',
-			'internalcode' =>'mcusli."config"',
-			'label' =>'Customer list position',
-			'type' => 'string',
-		),
-		'customer.lists.position' => array(
-			'code' =>'customer.lists.position',
-			'internalcode' =>'mcusli."pos"',
-			'label' =>'Customer list position',
-			'type' => 'int',
-		),
-		'customer.lists.status' => array(
-			'code' =>'customer.lists.status',
-			'internalcode' =>'mcusli."status"',
-			'label' =>'Customer list status',
-			'type' => 'int',
-		),
-		'customer.lists.ctime' => array(
-			'code' =>'customer.lists.ctime',
-			'internalcode' =>'mcusli."ctime"',
-			'label' =>'Customer list create date/time',
-			'type' => 'datetime',
-		),
-		'customer.lists.mtime' => array(
-			'code' =>'customer.lists.mtime',
-			'internalcode' =>'mcusli."mtime"',
-			'label' =>'Customer list modification date/time',
-			'type' => 'datetime',
-		),
-		'customer.lists.editor' => array(
-			'code' =>'customer.lists.editor',
-			'internalcode' =>'mcusli."editor"',
-			'label' =>'Customer list editor',
-			'type' => 'string',
-		),
-	);
-
-
-	/**
-	 * Removes old entries from the storage.
-	 *
-	 * @param iterable $siteids List of IDs for sites whose entries should be deleted
-	 * @return \Aimeos\MShop\Common\Manager\Iface Same object for fluent interface
-	 */
-	public function clear( iterable $siteids ) : \Aimeos\MShop\Common\Manager\Iface
-	{
-		$path = 'mshop/customer/manager/lists/submanagers';
-		foreach( $this->context()->config()->get( $path, ['type'] ) as $domain ) {
-			$this->object()->getSubManager( $domain )->clear( $siteids );
-		}
-
-		return $this->clearBase( $siteids, 'mshop/customer/manager/lists/laravel/delete' );
-	}
-
-
-	/**
-	 * Returns the list attributes that can be used for searching.
-	 *
-	 * @param bool $withsub Return also attributes of sub-managers if true
-	 * @return array List of attribute items implementing \Aimeos\Base\Criteria\Attribute\Iface
-	 */
-	public function getSearchAttributes( bool $withsub = true ) : array
-	{
-		$path = 'mshop/customer/manager/lists/submanagers';
-
-		return $this->getSearchAttributesBase( $this->searchConfig, $path, [], $withsub );
-	}
-
-
 	/**
 	 * Returns a new manager for customer extensions
 	 *
@@ -164,23 +34,12 @@ class Laravel
 
 
 	/**
-	 * Returns the config path for retrieving the configuration values.
+	 * Returns the name of the used table
 	 *
-	 * @return string Configuration path (mshop/customer/manager/lists/laravel/)
+	 * @return string Table name
 	 */
-	protected function getConfigPath() : string
+	protected function getTable() : string
 	{
-		return 'mshop/customer/manager/lists/laravel/';
-	}
-
-
-	/**
-	 * Returns the search configuration for searching items.
-	 *
-	 * @return array Associative list of search keys and search definitions
-	 */
-	protected function getSearchConfig() : array
-	{
-		return $this->searchConfig;
+		return 'users_list';
 	}
 }
