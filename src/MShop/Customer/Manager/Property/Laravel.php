93ee716e3fdf04e366e7e462f280dc1c9f6d4000
Aimeos <aimeos@aimeos.org>
2024-08-31 17:48:23 +0200

    Simplified customer property manager

--- src/MShop/Customer/Manager/Property/Laravel.php
+++ src/MShop/Customer/Manager/Property/Laravel.php
@@ -12,142 +12,35 @@ namespace Aimeos\MShop\Customer\Manager\Property;
 
 
 /**
- * Default property manager implementation.
+ * Laravel customer property manager implementation.
  *
  * @package MShop
  * @subpackage Customer
  */
 class Laravel
 	extends \Aimeos\MShop\Customer\Manager\Property\Standard
+	implements \Aimeos\MShop\Customer\Manager\Property\Iface
 {
-	private array $searchConfig = array(
-		'customer.property.id' => array(
-			'code' => 'customer.property.id',
-			'internalcode' => 'mcuspr."id"',
-			'internaldeps' => ['LEFT JOIN "users_property" AS mcuspr ON ( mcuspr."parentid" = mcus."id" )'],
-			'label' => 'Property ID',
-			'type' => 'int',
-			'public' => false,
-		),
-		'customer.property.parentid' => array(
-			'code' => 'customer.property.parentid',
-			'internalcode' => 'mcuspr."parentid"',
-			'label' => 'Property parent ID',
-			'type' => 'int',
-			'public' => false,
-		),
-		'customer.property.siteid' => array(
-			'code' => 'customer.property.siteid',
-			'internalcode' => 'mcuspr."siteid"',
-			'label' => 'Property site ID',
-			'type' => 'string',
-			'public' => false,
-		),
-		'customer.property.type' => array(
-			'code' => 'customer.property.type',
-			'internalcode' => 'mcuspr."type"',
-			'label' => 'Property type',
-			'type' => 'string',
-		),
-		'customer.property.value' => array(
-			'code' => 'customer.property.value',
-			'internalcode' => 'mcuspr."value"',
-			'label' => 'Property value',
-			'type' => 'string',
-		),
-		'customer.property.languageid' => array(
-			'code' => 'customer.property.languageid',
-			'internalcode' => 'mcuspr."langid"',
-			'label' => 'Property language ID',
-			'type' => 'string',
-		),
-		'customer.property.ctime' => array(
-			'code' => 'customer.property.ctime',
-			'internalcode' => 'mcuspr."ctime"',
-			'label' => 'Property create date/time',
-			'type' => 'datetime',
-			'public' => false,
-		),
-		'customer.property.mtime' => array(
-			'code' => 'customer.property.mtime',
-			'internalcode' => 'mcuspr."mtime"',
-			'label' => 'Property modify date',
-			'type' => 'datetime',
-			'public' => false,
-		),
-		'customer.property.editor' => array(
-			'code' => 'customer.property.editor',
-			'internalcode' => 'mcuspr."editor"',
-			'label' => 'Property editor',
-			'type' => 'string',
-			'public' => false,
-		),
-	);
-
-
-	/**
-	 * Removes old entries from the storage.
-	 *
-	 * @param iterable $siteids List of IDs for sites whose entries should be deleted
-	 * @return \Aimeos\MShop\Common\Manager\Iface Same object for fluent interface
-	 */
-	public function clear( iterable $siteids ) : \Aimeos\MShop\Common\Manager\Iface
-	{
-		$path = 'mshop/customer/manager/property/submanagers';
-		foreach( $this->context()->config()->get( $path, ['type'] ) as $domain ) {
-			$this->object()->getSubManager( $domain )->clear( $siteids );
-		}
-
-		return $this->clearBase( $siteids, 'mshop/customer/manager/property/laravel/delete' );
-	}
-
-
-	/**
-	 * Returns the attributes that can be used for searching.
-	 *
-	 * @param bool $withsub Return also attributes of sub-managers if true
-	 * @return array Returns a list of attribtes implementing \Aimeos\Base\Criteria\Attribute\Iface
-	 */
-	public function getSearchAttributes( bool $withsub = true ) : array
-	{
-		$path = 'mshop/customer/manager/property/submanagers';
-
-		return $this->getSearchAttributesBase( $this->searchConfig, $path, [], $withsub );
-	}
-
-
 	/**
 	 * Returns a new manager for customer extensions
 	 *
 	 * @param string $manager Name of the sub manager type in lower case
-	 * @param string|null $name Name of the implementation, will be from
-	 * configuration (or Default) if null
-	 * @return \Aimeos\MShop\Common\Manager\Iface Manager for different extensions, e.g property types, property lists etc.
+	 * @param string|null $name Name of the implementation, will be from configuration (or Default) if null
+	 * @return mixed Manager for different extensions, e.g stock, tags, locations, etc.
 	 */
 	public function getSubManager( string $manager, string $name = null ) : \Aimeos\MShop\Common\Manager\Iface
 	{
-		return $this->getSubManagerBase( 'customer', 'property/' . $manager, ( $name === null ? 'Laravel' : $name ) );
+		return parent::getSubManager( $manager, $name ?: 'Laravel' );
 	}
 
 
 	/**
-	 * Returns the config path for retrieving the configuration values.
+	 * Returns the name of the used table
 	 *
-	 * @return string Configuration path
+	 * @return string Table name
 	 */
-	protected function getConfigPath() : string
+	protected function getTable() : string
 	{
-		return 'mshop/customer/manager/property/laravel/';
-	}
-
-
-	/**
-	 * Returns the search configuration for searching items.
-	 *
-	 * @return array Associative list of search keys and search definitions
-	 */
-	protected function getSearchConfig() : array
-	{
-		return $this->searchConfig;
+		return 'users_property';
 	}
 }
